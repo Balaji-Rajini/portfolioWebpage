d657c733a01c72bde622a172159fbf160868b614
Balaji-Rajini <balajitheprogrammer@gmail.com>
2025-08-29 10:23:18 +0530

    verel errors solved

--- App.jsx
+++ App.jsx
@@ -1,25 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Intro from "./components/Intro";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import TechStack from "./components/TechStack";
 
-
-// Example component
-
 function App() {
-  return (<div className="">
-    <BrowserRouter>
-      <Routes>
-        {/* ✅ Wrap Route inside Routes & use capitalized component */}
-        <Route path="/" element={<><Intro/> , <About/><TechStack/> <Contact/></>} />
-                <Route path="/" element={<About/>} />
-        <Route path="/" element={<TechStack/>} />
+  return (
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          {/* ✅ Show all components on home page */}
+          <Route path="/" element={<><Intro /><About /><TechStack /><Contact /></>} />
 
-      </Routes>
-    </BrowserRouter>
+          {/* ✅ OR use separate pages */}
+          
+          <Route path="/about" element={<About />} />
+          <Route path="/techstack" element={<TechStack />} />
+          <Route path="/contact" element={<Contact />} />
+        
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Intro.jsx
@@ -0,0 +1,33 @@
+function Intro() {
+  return (
+    <>
+      {" "}
+      <div className="bg-[#47d3ef88] p-10 flex flex-col items-center ">
+       
+        <div className=" inline-flex  text-3xl font-mono  ">
+          <h1 className="text-5xl font-bold   transition duration-300 ease-in-out hover:drop-shadow-lg">
+            Welcome to My Portfolio Webpage...
+          </h1>
+        </div>
+
+<div className="grid lg:grid-cols-2 sm:grid-cols-1  ">
+        <div className=" hover:scale-110 duration-200 overflow-hidden w-60 mt-12 h-80 border rounded-2xl">
+          <img
+            src="src\assets\profimgcrop.jpg"
+            alt="image here"
+            className=" object-cover rounded-lg  "
+          />
+    </div>
+    <div>
+          <h1 className="text-4xl  font-bold  mt-20 hover:text-gray-800  transition duration-300 ease-in-out hover:drop-shadow-lg">Balaji R </h1>
+<br/>
+          <p className="text-xl">
+            Student | A Full Stack Web Developer | Java Enthusiast | Debug Pro
+          </p>
+     </div>
+      </div>
+      </div>  
+    </>
+  );
+}
+export default Intro;
